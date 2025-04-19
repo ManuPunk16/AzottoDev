2d0662bc20d44642cb66c75d55781c2bfdd7132d
Luis Adrian Hernandez Soto <luisadrianhdzsoto@gmail.com>
2025-04-19 10:14:41 -0600

    Reapply "feat: integrate Firebase and enhance projects component"
    This reverts commit d01e6714fc38bc8aff8c8833a01dc2fe6ad96aea.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Project } from '../../models/project.model';
+import { NgFor, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   imports: [
-
+    NgFor,
+    NgIf,
+    RouterLink,
+    DatePipe
   ],
   standalone: true,
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  projects: Project[] = [];
+  featuredProjects: Project[] = [];
+  loading: boolean = true;
+  error: boolean = false;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Project[]>('/assets/projects.json').subscribe({
+      next: (data) => {
+        this.projects = data;
+        this.featuredProjects = data.filter(project => project.featured);
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error cargando proyectos', err);
+        this.error = true;
+        this.loading = false;
+      }
+    });
+  }
 }
