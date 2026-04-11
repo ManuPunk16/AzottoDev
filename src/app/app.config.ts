0a0ccc54dc089f36816cd5c5e763b5c0b53f4baf
Luis Hernandez <luisadrianhdzsoto@gmail.com>
2026-04-10 19:07:07 -0600

    refactor: remove unused import for provideAnimationsAsync in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,6 @@ import {
   isDevMode,
 } from '@angular/core';
 import { provideRouter, withInMemoryScrolling, withRouterConfig } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
